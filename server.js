7bbac8d64789abff9b7dcc1cc1eb8f0afc99e679
mosetf <stanlym315@gmail.com>
2024-05-24 19:24:34 +0300

    Add API endpoint for fetching random quotes

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const axios = require('axios');
+const res = require('express/lib/response');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+async function getQuote(){
+    try {
+        const apikey = process.env.QUOTES_REST_API_KEY;
+        const response = await axios.get('https://quotes.rest/quote/random',{
+            headers: {
+                'Authorization': `Bearer ${apikey}`
+            }
+        });
+        const data = response.data;
+        const quoteText = data.content;
+        const quoteAuthor = data.author;
+        return{ quoteText, quoteAuthor};
+    }catch(error) {
+        console.error(error);
+        return{ quoteText:'Error fetching quote', quoteAuthor: ""};
+    }
+}
+
+app.get('/api/quote', async (req, res) => {
+    const quote = await getQuote();
+    res.json(quote);
+});
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
