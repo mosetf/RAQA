15c81b345b25a89f690036448d858995bc16c861
mosetf <stanlym315@gmail.com>
2024-06-29 14:43:58 +0300

    chore: Remove useCreateIndex option in connectDB function

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ async function connectDB() {
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useCreateIndex: true,
         });
         console.log('MongoDB connected');
     } catch (error) {
