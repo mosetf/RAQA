df166f10db9b0f0b7f6cbc2c327bbae172545082
mosetf <stanlym315@gmail.com>
2024-07-02 23:56:41 +0300

    chore: Update bcrypt dependency for password hashing and comparison

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 
 router.use(express.json());
 
@@ -55,10 +55,6 @@ router.post('/login', async (req, res) => {
     }
 
     console.log('Stored Hashed Password for user:', user.password);
-
-    // Hash the login attempt password for logging purposes
-    const hashedLoginPassword = await bcrypt.hash(password, 10);
-    console.log('Hashed Login Password (for logging):', hashedLoginPassword);
 
     const isMatch = await bcrypt.compare(password, user.password);
     console.log('Password Match result for email:', email, 'is', isMatch);
