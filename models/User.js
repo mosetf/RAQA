8719885d0afde54b27ff2da2883b027834d3812c
mosetf <stanlym315@gmail.com>
2024-07-03 00:58:56 +0300

    chore: Refactor User model to remove password hashing middleware

--- models/User.js
+++ models/User.js
@@ -18,14 +18,4 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-// Hash password before saving (middleware)
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-    return next();
-  }
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
-
 module.exports = mongoose.model('User', userSchema);
