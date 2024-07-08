83fee746aa0476d31edc61f07178f8f02b2d5b2e
mosetf <stanlym315@gmail.com>
2024-07-08 14:54:06 +0300

    chore: Update bcrypt salt rounds to 7 in User.js
    This commit updates the bcrypt salt rounds from 10 to 7 in the `User.js` file. By reducing the number of salt rounds, the password hashing process will be faster while still maintaining a reasonable level of security.

--- models/User.js
+++ models/User.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema({
 // Hash password before saving the user model
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
-  const salt = await bcrypt.genSalt(10);
+  const salt = await bcrypt.genSalt(7);
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
