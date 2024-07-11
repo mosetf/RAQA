ec25128556de1f52cf3508662a37784f4d8ed862
mosetf <stanlym315@gmail.com>
2024-07-11 15:25:54 +0300

    Refactor authentication and registration routes for improved code organization and separation of concerns

--- routes/routes.js
+++ routes/routes.js
@@ -1,57 +1,24 @@
 require('dotenv').config();
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const authRoutes = require('./authentication/authRoutes');
+const regRoutes = require('./registration/regRoutes');
+const { sendPasswordResetEmail } = require('../utils/mailer');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-const passport = require('passport');
-const { sendPasswordResetEmail } = require('../utils/mailer');
 
 const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret';
 
 router.use(express.json());
 
-router.post('/register', async (req, res) => {
-    const { username, email, password } = req.body;
+// Authentication Routes
+router.use('/auth', authRoutes);
 
-    if (!username || !email || !password) {
-        return res.status(400).send('Please fill in all fields');
-    }
+// Registration Routes
+router.use('/register', regRoutes);
 
-    try {
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(400).send('Email already in use');
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 7);
-        const newUser = new User({ username, email, password: hashedPassword });
-        await newUser.save();
-
-        res.status(201).send('Registration successful!');
-    } catch (err) {
-        res.status(500).send('Internal server error');
-    }
-});
-
-router.post('/login', (req, res, next) => {
-    passport.authenticate('local', (err, user, info) => {
-        if (err) {
-            return res.status(500).json({ success: false, message: 'Internal server error' });
-        }
-        if (!user) {
-            return res.status(401).json({ success: false, message: info.message });
-        }
-        req.logIn(user, (err) => {
-            if (err) {
-                return res.status(500).json({ success: false, message: 'Internal server error' });
-            }
-            const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: '1h' });
-            res.status(200).json({ success: true, token, user: { id: user._id, username: user.username, email: user.email } });
-        });
-    })(req, res, next);
-});
-
+// Forgot Password Route
 router.post('/forgot_password', async (req, res) => {
     const { email } = req.body;
 
@@ -72,6 +39,7 @@ router.post('/forgot_password', async (req, res) => {
     }
 });
 
+// Reset Password Route
 router.post('/reset_password', async (req, res) => {
     const { password, confirmPassword, token } = req.body;
 
