c4a837c559f83cf780b651364a8be31545c0b8cc
mosetf <stanlym315@gmail.com>
2024-07-11 15:33:09 +0300

    refactor: Add registration service functions
    The `regService.js` file is added to handle user registration-related functions. This includes implementing the `registerUser`, `findUserByEmail`, and `updateUserPassword` functions. These changes improve the organization and separation of concerns in the codebase.

---
+++ controllers/registration/regService.js
@@ -0,0 +1,18 @@
+const User = require('../../models/User');
+
+exports.registerUser = async (userData) => {
+    const user = new User(userData);
+    await user.save();
+    return { message: 'Registration successful' };
+};
+
+exports.findUserByEmail = async (email) => {
+    return await User.findOne({ email });
+};
+
+exports.updateUserPassword = async (userId, newPassword) => {
+    const user = await User.findById(userId);
+    user.password = newPassword;
+    await user.save();
+    return { message: 'Password updated successfully' };
+};
