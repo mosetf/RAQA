05a877d9cb6b4c8bfecb83a5b6a65a9e6bbc8cf6
mosetf <stanlym315@gmail.com>
2024-07-11 15:31:34 +0300

    chore: Import connectDB function and remove unnecessary code

--- server.js
+++ server.js
@@ -1,38 +1,15 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
 const axios = require('axios');
 const path = require('path');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const routes = require('./routes/routes');
 const passport = require('passport');
+const connectDB = require('./config/db'); // Import the connectDB function
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-/**
- * Connects to the MongoDB database.
- * @async
- * @function connectDB
- * @throws {Error} If there is an error connecting to the database.
- * @returns {Promise<void>} A promise that resolves when the database connection is successful.
- */
-async function connectDB() {
-    const dbUri = process.env.MONGO_URI;
-    if (!dbUri) {
-        console.error('MongoDB connection error: MONGO_URI is not defined in .env');
-        process.exit(1);
-    }
-
-    try {
-        await mongoose.connect(dbUri);
-        console.log('MongoDB connected');
-    } catch (error) {
-        console.error('MongoDB connection error:', error);
-        process.exit(1);
-    }
-}
 
 /**
  * Retrieves a random quote from the 'https://api.quotable.io/quotes/random' API.
