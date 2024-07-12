bff916befc15e5ab1e5da0083519a45a5f2f6221
mosetf <stanlym315@gmail.com>
2024-07-13 01:57:19 +0300

    refactor: Add logger utility for improved logging

--- controllers/registration/regController.js
+++ controllers/registration/regController.js
@@ -1,10 +1,14 @@
 const regService = require('./regService');
+const logger = require('../../utils/logger');
 
 exports.register = async (req, res) => {
+    logger.info('Attempting to register a new user');
     try {
         const response = await regService.registerUser(req.body);
+        logger.info('Registration successful');
         res.json(response);
     } catch (error) {
-        res.status(500).json({ error: 'Registration failed' });
+        logger.error('Registration failed: ' + error.message);
+        res.status(500).json({ error: 'Registration failed', message: error.message });
     }
 };
