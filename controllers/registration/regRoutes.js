92734607f2977fc4074cab3abb0bcaafee288379
mosetf <stanlym315@gmail.com>
2024-07-13 00:44:22 +0300

    refactor: Remove registration route
    The `regRoutes.js` file has been removed from the project as it is no longer needed. This improves code organization and removes unnecessary code.

--- controllers/registration/regRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const regController = require('./regController');
-const router = express.Router();
-
-router.post('/register', regController.register);
-
-module.exports = router;
