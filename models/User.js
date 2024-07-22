726a01ff04a8e0c65f9d9a74d6fdf9699d02b266
mosetf <stanlym315@gmail.com>
2024-07-22 14:40:44 +0300

    refactor: Add resetPasswordToken and resetPasswordExpires fields to User model

--- models/User.js
+++ models/User.js
@@ -16,6 +16,8 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
 });
 
 // Hash password before saving the user model
