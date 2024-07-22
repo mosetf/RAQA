d59193a217c90ef56303edcc9ca036c5318ded52
mosetf <stanlym315@gmail.com>
2024-07-22 14:40:16 +0300

    refactor: Add password reset functionality to authService.js

--- controllers/authentication/authService.js
+++ controllers/authentication/authService.js
@@ -15,3 +15,19 @@ exports.logout = (req) => {
 exports.findUserByEmail = async (email) => {
     return await User.findOne({ email });
 };
+
+exports.savePasswordResetToken = async (userId, token) => {
+    // Save the token in the database with an expiration time
+    await User.updateOne({ _id: userId }, { resetPasswordToken: token, resetPasswordExpires: Date.now() + 3600000 }); // 1 hour
+};
+
+exports.verifyPasswordResetToken = async (token) => {
+    const user = await User.findOne({ resetPasswordToken: token, resetPasswordExpires: { $gt: Date.now() } });
+    return user ? user._id : null;
+};
+
+exports.updatePassword = async (userId, newPassword) => {
+    const user = await User.findById(userId);
+    user.password = newPassword; // Assume you hash the password before saving
+    await user.save();
+};
