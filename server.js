d60723e733207f088801aaedae2435392514e238
mosetf <stanlym315@gmail.com>
2024-07-22 21:44:51 +0300

    refactor: Update server.js to improve middleware setup and serve React app

--- server.js
+++ server.js
@@ -12,6 +12,19 @@ const logger = require('./utils/logger');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Middleware setup
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'client/public')));
+app.use(session({
+    secret: process.env.SESSION_SECRET || 'your_secret_key',
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URL })
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Function to fetch a random quote
 async function getQuote() {
     try {
         const response = await axios.get('https://api.quotable.io/quotes/random');
@@ -23,35 +36,27 @@ async function getQuote() {
     }
 }
 
+// API route to get a random quote
 app.get('/api/quote', async (req, res) => {
     const quote = await getQuote();
     res.json(quote);
 });
 
-//app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'client/build')));
+// Serve the React app
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+  res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
+});
 
-app.use(express.json());
-app.use(session({
-    secret: process.env.SESSION_SECRET || 'your_secret_key',
-    resave: false,
-    saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: process.env.MONGO_URL })
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
+// API routes
 app.use('/api', routes);
 
+// Error handling middleware
 app.use((err, req, res, next) => {
     logger.error(err.message);
     res.status(500).send('Something went wrong');
 });
 
+// Connect to the database and start the server
 connectDB().then(() => {
     app.listen(port, () => logger.info(`Server listening on port ${port}`));
 });
