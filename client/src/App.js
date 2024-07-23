27f78e4923f39cc724e55251e798b5931101e2b7
mosetf <stanlym315@gmail.com>
2024-07-23 20:24:18 +0300

    refactor: Update import paths and component names for forgot password and reset password

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ForgotPassword from '../src/components/forgotpassword';
-import ResetPassword from '../src/components/resetpassword';
+import ForgotPassword from './components/forgotpassword';
+import ResetPassword from './components/ResetPassword';
 
 const App = () => {
   return (
