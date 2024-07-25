cd67ac2b72c838582ce23833f5a171b8b4931609
mosetf <stanlym315@gmail.com>
2024-07-25 15:12:30 +0300

    refactor: Update mailer.js to use environment variables for mail host and port

--- utils/mailer.js
+++ utils/mailer.js
@@ -2,7 +2,8 @@ const nodemailer = require('nodemailer');
 
 
 let transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
