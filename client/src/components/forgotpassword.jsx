1cf695cb32c1fc2e2e1a16b16c80157e0ed41b96
mosetf <stanlym315@gmail.com>
2024-08-08 00:29:59 +0300

    refactor: Update forgot password form CSS file name to lowercase

--- client/src/components/forgotpassword.jsx
+++ client/src/components/forgotpassword.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './ForgotPassword.css';
+import './forgotpassword.css';
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState('');
