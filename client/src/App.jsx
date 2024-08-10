63751244ddeffbcf741a6ff620912e5e5eb2b89d
mosetf <stanlym315@gmail.com>
2024-08-10 22:59:46 +0300

    refactor: Add HomePage component and route for /home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './components/mainPage';
 import ForgotPassword from './components/forgotpassword';
 import ResetPassword from './components/resetpassword';
+import HomePage from './components/HomePage';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/home" component={HomePage} />
         <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
     </Router>
