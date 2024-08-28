9e69346cb48e51b1a16e33df48d093881116e41b
mosetf <stanlym315@gmail.com>
2024-08-29 02:20:25 +0300

    refactor: Add UserPage component and route for /user

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,8 @@ import MainPage from './components/mainPage';
 import ForgotPassword from './components/forgotpassword';
 import ResetPassword from './components/resetpassword';
 import HomePage from './components/HomePage';
+import UserPage from './components/UserPage';
+import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => {
   return (
@@ -11,8 +13,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/home" component={HomePage} />
         <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/home" element={<ProtectedRoute element={HomePage} />} />
+        <Route path="/user" element={<ProtectedRoute element={UserPage} />} />
       </Routes>
     </Router>
   );
