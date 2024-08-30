bb703bdfa8405dde32d51170cee95983ca8844d3
mosetf <stanlym315@gmail.com>
2024-08-30 17:15:46 +0300

    chore: Create Header component for the Quote Generator Dashboard

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Quote Generator Dashboard</h1>
+      <button className="logout-button">Logout</button>
+    </div>
+  );
+};
+
+export default Header;
