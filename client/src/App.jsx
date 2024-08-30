72ad8fd771f1fe79d32653b0bf67d91ea481beb7
mosetf <stanlym315@gmail.com>
2024-08-30 17:14:23 +0300

    chore: Update App component to include Dashboard route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './components/mainPage';
 import ForgotPassword from './components/forgotpassword';
 import ResetPassword from './components/resetpassword';
-import HomePage from './components/HomePage';
 import UserPage from './components/UserPage';
 import ProtectedRoute from './components/ProtectedRoute';
+import Dashboard from './components/Dashboard';
 
 const App = () => {
   return (
@@ -14,11 +14,11 @@ const App = () => {
         <Route path="/" element={<MainPage />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/home" element={<ProtectedRoute element={HomePage} />} />
         <Route path="/user" element={<ProtectedRoute element={UserPage} />} />
+        <ProtectedRoute path="/dashboard" component={Dashboard} />
       </Routes>
     </Router>
   );
 };
 
-export default App;
+export default App; 
