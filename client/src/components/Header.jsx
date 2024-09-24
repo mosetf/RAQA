318b5e9ed9a39611dea085a3f85f94096169c047
mosetf <stanlym315@gmail.com>
2024-09-25 00:16:33 +0300

    Refactor Header component to conditionally render sidebar toggle button based on user authentication

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Box, Avatar, Typography, IconButton } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu'; // Import the MenuIcon for the hamburger
 
-const Header = ({ toggleSidebar }) => {
+const Header = ({ toggleSidebar, isAuthenticated }) => {
   return (
     <Box display="flex" alignItems="center" justifyContent="space-between" p={2} bgcolor="primary.main" color="white">
-      <IconButton onClick={toggleSidebar} color="inherit"> {/* Button to toggle the sidebar */}
-        <MenuIcon />
-      </IconButton>
+      {isAuthenticated && (
+        <IconButton onClick={toggleSidebar} color="inherit"> {/* Button to toggle the sidebar */}
+          <MenuIcon />
+        </IconButton>
+      )}
       <Box display="flex" alignItems="center" gap={2}>
         <Avatar src="/avatar.png" sx={{ width: 56, height: 56 }} />
         <Box>
