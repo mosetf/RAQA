b403fc4cee4b8e60c313e0cc585933f2447bb0ab
mosetf <stanlym315@gmail.com>
2024-09-25 00:02:51 +0300

    Refactor Header component to include a sidebar toggle button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Box, Avatar, Typography } from '@mui/material';
+import { Box, Avatar, Typography, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu'; // Import the MenuIcon for the hamburger
 
-const Header = () => {
+const Header = ({ toggleSidebar }) => {
   return (
-    <Box display="flex" alignItems="center" gap={2} p={2} bgcolor="primary.main" color="white">
-      <Avatar src="/avatar.png" sx={{ width: 56, height: 56 }} />
-      <Box>
-        <Typography variant="h6" fontWeight="bold">
-          Duck UI
-        </Typography>
-        <Typography variant="body2">
-          Duckui@demo.com
-        </Typography>
+    <Box display="flex" alignItems="center" justifyContent="space-between" p={2} bgcolor="primary.main" color="white">
+      <IconButton onClick={toggleSidebar} color="inherit"> {/* Button to toggle the sidebar */}
+        <MenuIcon />
+      </IconButton>
+      <Box display="flex" alignItems="center" gap={2}>
+        <Avatar src="/avatar.png" sx={{ width: 56, height: 56 }} />
+        <Box>
+          <Typography variant="h6" fontWeight="bold">
+            Raqa
+          </Typography>
+          <Typography variant="body2">Hello</Typography>
+        </Box>
       </Box>
     </Box>
   );
