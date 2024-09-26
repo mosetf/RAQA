fc8f7a59d076fc6e03fe514f42aee1c10650eafe
mosetf <stanlym315@gmail.com>
2024-09-26 18:02:27 +0300

    Refactor routes.js to include file upload functionality and update user details route

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path');
 const router = express.Router();
 const authController = require('../controllers/authentication/authController');
+const userController = require('../controllers/userController');
 const regController = require('../controllers/registration/regController');
 const logger = require('../utils/logger');
 const Quote = require('../models/Quote'); 
+const authMiddleware = require('../middleware/authMiddleware');
 
+
+
+// Configure multer for file uploads
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
 
 // Middleware to parse JSON bodies
 router.use(express.json());
@@ -30,5 +47,10 @@ router.post('/login', authController.login);
 router.post('/register', regController.register);
 router.post('/forgot-password', authController.forgotPassword);
 router.post('/reset-password', authController.resetPassword);
+
+
+
+// Update user details route
+router.post('/update-user', authMiddleware, upload.single('profilePicture'), userController.updateUserDetails);
 
 module.exports = router;
