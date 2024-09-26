c6ad5b391149f30e95f44b02e1cf0366008bc6e0
mosetf <stanlym315@gmail.com>
2024-09-26 19:17:31 +0300

    Refactor authMiddleware.js to handle token authentication and user authorization

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,28 +3,28 @@ const User = require('../models/User');
 
 
 const authMiddleware = async (req, res, next) => {
-    const token = req.header('Authorization')?.replace('Bearer ', '');
-    console.log('Token:', token); // Log the token
-  
-    if (!token) {
-      return res.status(401).json({ error: 'No token provided' });
+  const token = req.header('Authorization')?.replace('Bearer ', '');
+  console.log('Token:', token); // Log the token
+
+  if (!token) {
+    return res.status(401).json({ error: 'No token provided' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log('Decoded:', decoded); // Log the decoded token
+
+    const user = await User.findById(decoded.id);
+    if (!user) {
+      return res.status(401).json({ error: 'User not found' });
     }
-  
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      console.log('Decoded:', decoded); // Log the decoded token
-  
-      const user = await User.findById(decoded.id);
-      if (!user) {
-        return res.status(401).json({ error: 'User not found' });
-      }
-  
-      req.user = user;
-      next();
-    } catch (error) {
-      console.error('Error:', error); // Log the error
-      res.status(401).json({ error: 'Invalid token' });
-    }
-  };
-  
-  module.exports = authMiddleware;
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error('Error:', error); // Log the error
+    res.status(401).json({ error: 'Invalid token' });
+  }
+};
+
+module.exports = authMiddleware;
