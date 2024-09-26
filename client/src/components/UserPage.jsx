a1a40d93920f11b43124c6d0d112ec47081ba059
mosetf <stanlym315@gmail.com>
2024-09-26 19:17:54 +0300

    Refactor UserPage component to display user's profile picture

--- client/src/components/UserPage.jsx
+++ client/src/components/UserPage.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Box, Typography, Button } from '@mui/material';
+import { Box, Typography, Avatar, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 const moods = ['Happy', 'Sad', 'Motivated', 'Relaxed'];
 
-const UserPage = () => {
+const UserPage = ({ user }) => {
   return (
     <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" height="100vh" bgcolor="grey.100">
       <Typography variant="h4" gutterBottom>
         Select Your Mood
       </Typography>
+      <Avatar src={user.profilePicture || "/avatar.png"} sx={{ width: 100, height: 100 }} />
       <Box display="flex" flexDirection="column" alignItems="center" gap={2}>
         {moods.map((mood) => (
           <Button
